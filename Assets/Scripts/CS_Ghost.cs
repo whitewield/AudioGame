e8759ff72d235b2040636daedaac1e3224df993b
Hang <whitewield@126.com>
2017-04-05 20:36:14 -0400

    Ghost

---
+++ Assets/Scripts/CS_Ghost.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Ghost : MonoBehaviour {
+	enum Status {
+		Idle,
+		Find
+	}
+
+	[SerializeField] float myVelocity = 3;
+	private Status myStatus = Status.Idle;
+	private Vector3 myDirection;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateFind ();
+
+		UpdateMove ();
+	}
+
+	private void UpdateMove () {
+		if (myStatus == Status.Find) {
+			myDirection = (CS_GhostTarget.Instance.transform.position - this.transform.position).normalized;
+			this.GetComponent<Rigidbody> ().velocity = myDirection * myVelocity;
+		}
+	}
+
+	private void UpdateFind () {
+		float myVisionDistance = 50;
+		Ray ray = new Ray (this.transform.position, CS_GhostTarget.Instance.transform.position - this.transform.position);
+		RaycastHit hit;
+		//		int t_layerMask = (int) Mathf.Pow (2, 8); //for the layer you want to do the raycast
+		//		if (Physics.Raycast (ray, out hit, myVisionDistance, t_layerMask))
+		if (Physics.Raycast (ray, out hit, myVisionDistance))
+		if (hit.collider.tag == "Wall") {
+			myStatus = Status.Idle;
+		} else {
+			myStatus = Status.Find;
+		}
+	}
+}
---
+++ Assets/Scripts/CS_GhostTarget.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_GhostTarget : MonoBehaviour {
+
+	private static CS_GhostTarget instance = null;
+
+	//========================================================================
+	public static CS_GhostTarget Instance {
+		get { 
+			return instance;
+		}
+	}
+
+	void Awake () {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+		} else {
+			instance = this;
+		}
+//		DontDestroyOnLoad(this.gameObject);
+	}
+	//========================================================================
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
